11484d412c60bd95650804bc6731a750cbc16030
abazurek <skala2294@gmail.com>
2020-05-22 23:05:43 +0200

    home structure, done layout, without form validation and submit

--- src/components/Home/contact/Contact.js
+++ src/components/Home/contact/Contact.js
@@ -1,11 +1,43 @@
 import React from "react";
 
+import FooterSection from "./FooterSection";
+import decoration from './../../../assets/Decoration.svg'
+
 export default function Contact() {
 
-    return(
+
+    function submitFrom(e) {
+
+        e.preventDefault();
+    }
+
+    return (
         <section id='contact' className='contactSection'>
-            <div>
-                Contact
+            <div className='container'>
+                <div className='contactBox'>
+                    <div className='text'>
+                        <h2>Skontaktuj się z nami</h2>
+                        <img src={decoration} alt='decoration icon'/>
+                    </div>
+                    <form onSubmit={submitFrom}>
+                        <div className='inputsBox'>
+                            <label> Wpisz swoje imię
+                                <input type='text' placeholder='Krzysztof'/>
+                            </label>
+                            <label>Wpisz swój email
+                                <input type='text' placeholder='abc@xyz.pl'/>
+                            </label>
+                        </div>
+                        <label className='message'>Wpisz swoją wiadomość
+                            <textarea placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'/>
+                        </label>
+                        <div className='buttonBox'>
+                            <button type='submit'>Wyślij</button>
+                        </div>
+                    </form>
+                </div>
+                <FooterSection/>
             </div>
         </section>
     )
