5994a27a089f439a2555e4c18b592ce535addbaa
Alicja Zurek <skala2294@gmail.com>
2020-06-18 17:02:40 +0200

    changes on fourth step, llittle changes to do in second and third steps.

--- src/components/GiveThingsSite/stepsSections/FourthStep.js
+++ src/components/GiveThingsSite/stepsSections/FourthStep.js
@@ -5,33 +5,35 @@ export default function FourthStep({title}) {
     return(
         <div>
             <h2 className='step-title'>{title}</h2>
-            <form>
-                <h3>Adres obioru</h3>
-                <label> Ulica
-                    <input type='text'/>
-                </label>
-                <label> Miasto
-                    <input type='text'/>
-                </label>
-                <label> Kod pocztowy
-                    <input type='number'/>
-                </label>
-                <label> Numer telefonu
-                    <input type='number'/>
-                </label>
-            </form>
-            <form>
-                <h3>Termin odbioru</h3>
-                <label> Data
-                    <input type='date'/>
-                </label>
-                <label> Godzina
-                    <input type='time'/>
-                </label>
-                <label> Uwagi dla kuriera
-                    <textarea/>
-                </label>
-            </form>
+            <div className='fourth-forms box'>
+                <form>
+                    <h3>Adres obioru</h3>
+                    <label> <span>Ulica</span>
+                        <input type='text'/>
+                    </label>
+                    <label> <span>Miasto</span>
+                        <input type='text'/>
+                    </label>
+                    <label> <span>Kod pocztowy</span>
+                        <input type='number'/>
+                    </label>
+                    <label> <span>Numer telefonu</span>
+                        <input type='number'/>
+                    </label>
+                </form>
+                <form>
+                    <h3>Termin odbioru</h3>
+                    <label> <span>Data</span>
+                        <input type='date'/>
+                    </label>
+                    <label> <span>Godzina</span>
+                        <input type='time'/>
+                    </label>
+                    <label> <span>Uwagi dla kuriera</span>
+                        <textarea/>
+                    </label>
+                </form>
+            </div>
         </div>
     )
 }
