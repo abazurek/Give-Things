d5dd076cd9fcb8ec0983d446532c97ced0811069
Alicja Zurek <skala2294@gmail.com>
2020-07-09 18:45:57 +0200

    done thanks step with styling, little changes with footer styling and start view in give things page

--- src/components/GiveThingsSite/GiveThingsAll.js
+++ src/components/GiveThingsSite/GiveThingsAll.js
@@ -1,12 +1,14 @@
 import React from "react";
 import GiveThingsStart from "./GiveThingsStart";
 import StepsSection from "./stepsSections/StepsSection";
+import Contact from "../Home/contact/Contact";
 
 export default function GiveThingsAll() {
     return(
         <>
             <GiveThingsStart/>
             <StepsSection/>
+            <Contact/>
         </>
     )
 }
