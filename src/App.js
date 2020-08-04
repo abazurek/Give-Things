9e1df8515188619c9ddfa7f70f775d4eb18d3009
abazurek <skala2294@gmail.com>
2020-08-04 17:00:52 +0200

    history work but sited doesnt refresh.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Router, Route, Switch} from "react-router-dom";
 
 import Home from './components/Home/Home'
 import LoginSite from "./components/LogRegisterSites/LogSite";
@@ -7,10 +7,8 @@ import RegisterSite from "./components/LogRegisterSites/RegisterSite";
 import Header from "./components/Home/header/Header";
 import LogOut from "./components/LogOut";
 import GiveThingsAll from "./components/GiveThingsSite/GiveThingsAll";
-import {createBrowserHistory} from 'history'
 
-
-export  const history = createBrowserHistory();
+import history from "./history";
 
 
 function App() {
